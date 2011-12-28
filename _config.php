7fc939050ca6893c30a0895b634a373d6f123baf
Hugo Demiglio <hugo@formaweb.com.br>
2011-12-28 02:49:00 -0200

    first commit

---
+++ _config.php
@@ -0,0 +1,11 @@
+<?php
+
+$ordem = array('boot.hgc', 'declarations.hgc', 'functions.hgc', 'ethernet_headers.hgc', 'ethernet_functions.hgc', 'drawer_functions.hgc', 'leds_functions.hgc', 'setup.hgc', 'loop.hgc', 'loop_ethernet.hgc');
+
+$arquivo_saida = 'RESTcontroller.ino';
+
+$name = 'Hugo Leonardo Demiglio';
+$email = 'hugodemiglio@gmail.com';
+$version = '0.0.1';
+
+?>
